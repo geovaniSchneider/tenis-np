f55d9fda5895ccb1794d2f1ab25c386f687df1be
Geovani <schbrs@gmail.com>
2026-01-03 20:52:14 -0300

    ciclo 2026.1

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -50,14 +50,39 @@ export class HomeComponent implements OnInit {
     }
   ];
 
-  cicloAtual = '2025.3';
+  cicloAtual = '2026.1';
 
   grupos = [
-    { id: 1, nome: 'Classe 1', linkTabela: 'https://docs.google.com/spreadsheets/d/16b_qrElHNtM6zmaXuypyvh_s7ALStE56NDmLPV8JSNY/edit?usp=sharing', linkFormulario: 'https://forms.gle/T6XApYiUCTeyuS6T8' },
-    { id: 2, nome: 'Classe 2', linkTabela: 'https://docs.google.com/spreadsheets/d/1HFE6-eEeO0AuztuJUeeU3tpuaod22XT2htfohr0vduc/edit?usp=sharing', linkFormulario: 'https://forms.gle/R5fcizpCAZ3auaQ57' },
-    { id: 3, nome: 'Classe 3', linkTabela: 'https://docs.google.com/spreadsheets/d/1Qfn0NBKNYMMGEnrJtOSbg-UTX5lkcOYu_1YOa8sjUTs/edit?usp=sharing', linkFormulario: 'https://forms.gle/mMcpeXrYUKPV1G2d6' },
-    { id: 4, nome: 'Classe 4', linkTabela: 'https://docs.google.com/spreadsheets/d/1R1YsraZDGyirQk3271UkMI1R1YNjmkaCno7LXuUuoTI/edit?usp=sharing', linkFormulario: 'https://forms.gle/F1BEdxQsY8gsmrQj7' },
-    { id: 5, nome: 'Classe 5', linkTabela: 'https://docs.google.com/spreadsheets/d/1f5ykhp0odW9XXZ4oYFYnhocEpaF-G6gvRx-2Cb0XWU4/edit?usp=sharing', linkFormulario: 'https://forms.gle/M6FB2N14fqeHTz817' }
+    {
+      id: 1,
+      nome: 'Classe 1',
+      linkTabela: 'https://docs.google.com/spreadsheets/d/16sVqGlaUWUisO4yDFlHo_2W9r1VeFbLGeuBE4lTfxJM/edit?usp=sharing',
+      linkFormulario: 'https://docs.google.com/forms/d/e/1FAIpQLSdzmVWzGTFhMqdm4wFWOPWil5Rwxf3sDaR2QeKv3iOIY_DnPA/viewform?usp=sharing&ouid=118078082500890152692'
+    },
+    {
+      id: 2,
+      nome: 'Classe 2',
+      linkTabela: 'https://docs.google.com/spreadsheets/d/14_WhwtRMLVs3zdH0w26ZKzlxn2JkikhOVxWmRG0udCk/edit?usp=sharing',
+      linkFormulario: 'https://docs.google.com/forms/d/e/1FAIpQLSc-B2TnJu5_tO4X22dJxvjxDjVefplvIh9OMMS7w7VhapQg-g/viewform?usp=sharing&ouid=118078082500890152692'
+    },
+    {
+      id: 3,
+      nome: 'Classe 3',
+      linkTabela: 'https://docs.google.com/spreadsheets/d/1Oj8ytIhXBKX1ppDiPv2bF67y_PtaaqpfDfjgVun9eLw/edit?usp=sharing',
+      linkFormulario: 'https://docs.google.com/forms/d/e/1FAIpQLScrxuVvJf2lAK86Ak9aLvcRcoGAfTtQIEkioqCPs5SA8blKeg/viewform?usp=sharing&ouid=118078082500890152692'
+    },
+    {
+      id: 4,
+      nome: 'Classe 4',
+      linkTabela: 'https://docs.google.com/spreadsheets/d/1KphDrlv1_IOfquUJhO1SuE3UXYhFPMMLrBd5KXlsz-E/edit?usp=sharing',
+      linkFormulario: 'https://docs.google.com/forms/d/e/1FAIpQLSeB2iN9d-C_l4oEUWiTLs7EO3TPnuje2RU3-3Pgqtrnm8qhWg/viewform?usp=sharing&ouid=118078082500890152692'
+    },
+    {
+      id: 5,
+      nome: 'Classe 5',
+      linkTabela: 'https://docs.google.com/spreadsheets/d/1XdQqCh3qrlr16DFkivdJtBnbZ9IaqLRoCMLsdFJ8F2w/edit?usp=sharing',
+      linkFormulario: 'https://docs.google.com/forms/d/e/1FAIpQLSf4VHFsszcYkfYUNuVnyIyjtVhdS83RVU32wGeETTeLf65Upw/viewform?usp=sharing&ouid=118078082500890152692'
+    }
   ];
 
   // Índice inicial aleatório para o carrossel
